af00930cb1f5c340387c5a387c2482e6a90a8ead
Ben Baron <benjamin.m.baron@gmail.com>
2021-06-19 14:47:36 +0700

    first commit, made files

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+const mainContainer = document.getElementById('main-container');
+
+const movieArray = [{name: "The Matrix"}, {name: "Star Wars"}];
+
+
+
+
+  for (let i=0; i<movieArray.length; i++) {
+    const movie = document.createElement('div');
+    movie.classList.add('movie');
+    movie.innerText = 'hello';
+    mainContainer.appendChild(movie);
+    
+  }
